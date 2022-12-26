19a9d12d51c75917723853dfad1de2695f074a13
Brayan Puma <119136640+Karnicero@users.noreply.github.com>
2022-12-25 23:01:19 -0500

    Update semana11_2.py

--- sem11/semana11_2.py
+++ sem11/semana11_2.py
@@ -1,9 +1,8 @@
 """
 Diseña un algoritmo en el que se ingrese 2 digitos para restarlos
-y el programa los contabilice y los acumule usando el while.
+y el programa los contabilice usando el while.
 """
 #declaracion de variables
-acu = 0 
 cont = 0
 resta = 0
 #entrada
@@ -13,11 +12,9 @@ rpt = "si"
 while (rpt != "no"):
     num1 = int (input ("ingrese el primer digito: "))
     num2 = int (input ("ingrese el segundo digito: "))
-    acu = acu + num1 + num2
     cont = cont + 2
     resta = num1 - num2
     print ("La resta es: ", resta)
-    print("La acumulación de resta es de: " , acu)
     print("Contador de cuantos digitos ingresó: ", cont)
 
-    rpt = str (input ("Desea realizar otra resta? si/no : "))
+    rpt = str (input ("¿Desea realizar otra resta? si/no : "))
