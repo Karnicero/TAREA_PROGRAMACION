74a0814c117ec9915f85debcecf8dbfbfb18daae
Brayan Puma <119136640+Karnicero@users.noreply.github.com>
2022-12-25 23:26:12 -0500

    Update semana11_3.py

--- sem11/semana11_3.py
+++ sem11/semana11_3.py
@@ -1,20 +1,20 @@
-print("El alumno procedera a poner sus notas de IO, PC, EM, EB, el programa arrojara si aprobó o desaprobó")
-numero1 = float(input("Coloque la nota de IO...... "))
+print("El alumno procedera a poner 4 notas, el programa arrojara si aprobó o desaprobó")
+numero1 = float(input("Coloque la nota 1...... "))
 while numero1<0 or numero1>20:
     print("NOTA NO VALIDA")
-    numero1 = float(input("Coloque la nota de IO...... "))
-numero2 = float(input("Coloque la nota de PC...... "))
+    numero1 = float(input("Coloque la nota 1...... "))
+numero2 = float(input("Coloque la nota 2...... "))
 while numero2<0 or numero2>20:
     print("NOTA NO VALIDA")
-    numero2 = float(input("Coloque la nota de PC...... "))
-numero3 = float(input("Coloque la nota de EM...... "))
+    numero2 = float(input("Coloque la nota 2...... "))
+numero3 = float(input("Coloque la nota 3...... "))
 while numero3<0 or numero3>20:
     print("NOTA NO VALIDA")
-    numero3 = float(input("Coloque la nota de EM...... "))
-numero4 = float(input("Coloque la nota de EB...... "))
+    numero3 = float(input("Coloque la nota 3...... "))
+numero4 = float(input("Coloque la nota de 4...... "))
 while numero4<0 or numero4>20:
     print("NOTA NO VALIDA")
-    numero4 = float(input("Coloque la nota de EB...... "))
+    numero4 = float(input("Coloque la nota de 4...... "))
 
 PROMEDIO = (numero1+numero2+numero3+numero4)/4
 print("Su promedio final es: ",PROMEDIO )
