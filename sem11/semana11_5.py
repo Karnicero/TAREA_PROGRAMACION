54f6330b0ef0cdd95b2ec5821ac17428e2cff141
Brayan Puma <119136640+Karnicero@users.noreply.github.com>
2022-12-25 23:33:40 -0500

    Add files via upload

---
+++ sem11/semana11_5.py
@@ -0,0 +1,49 @@
+
+
+if __name__ == '__main__':
+	opc = str()
+	while True:
+		print(" MENU ")
+		print("----------------------")
+		print("(1) Arroz con pollo")
+		print("(2) Aji de gallina")
+		print("(3) Arroz chaufa")
+		print("(4) Seco de pollo")
+		print("(0) salir")
+		print("-----------------------")
+		print("Elige una opcion(0-4).......", end="")
+		opc = input()
+		if opc=="1":
+			print("") 
+			print("")
+			print("esta es la opcion 1")
+			print("!!Has elegido Arroz con pollo!!")
+			input() 
+		elif opc=="2":
+			print("")
+			print("")
+			print("esta es la opcion 2")
+			print("!!Has elegido Aji de gallina!!")
+			input()
+		elif opc=="3":
+			print("") 
+			print("")
+			print("esta es la opcion 3")
+			print("!!Has elegido Arroz chaufa!!")
+			input() 
+		elif opc=="4":
+			print("") 
+			print("")
+			print("esta es la opcion 4")
+			print("!!Has elegido Seco de pollo!!")
+			input() 
+		elif opc=="0":
+			print("")
+			print("Muchas gracias por su visita")
+		else:
+			print("")
+			print(opc,"no se una opcion correcta, Intentalo de nuevo")
+			print("Pulsa una tecla para continuar")
+			input()
+		if (opc=="0"): break
+
